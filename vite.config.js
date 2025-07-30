41a56e770ae6b78482f5dfc5481962f7c446acbb
raviendalpatadu <102800797+raviendalpatadu@users.noreply.github.com>
2025-07-30 19:42:04 +0530

    Update asset inclusion in Vite config to support SVG images

--- vite.config.js
+++ vite.config.js
@@ -9,5 +9,5 @@ export default defineConfig({
     port: 3000,
     host: true,
   },
-  assetsInclude: ['**/*.JPG', '**/*.jpeg', '**/*.png', '**/*.gif'], // Include image assets
+  assetsInclude: ['**/*.JPG', '**/*.jpg', '**/*.jpeg', '**/*.png', '**/*.svg'], // Include image assets
 })
